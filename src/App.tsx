cd9ba37b9cc86d199c2c5c50d13e6b0015a9608f
Makus14 <mesha.maksim14@gmail.com>
2025-01-15 13:58:53 +0300

    typescript with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 // import { Person } from "./components/Person";
 // import { PersonList } from "./components/PersonList";
-import { Greet } from "./components/Greet";
-import { Status } from "./components/Status";
-import { Heading } from "./components/Heading";
-import { Oscar } from "./components/Oscar";
+// import { Greet } from "./components/Greet";
+// import { Status } from "./components/Status";
+// import { Heading } from "./components/Heading";
+// import { Oscar } from "./components/Oscar";
+// import { Button } from "./components/Button";
+// import { Input } from "./components/Input";
+// import { Container } from "./components/Container";
 import "./App.css";
 
 function App() {
@@ -24,12 +27,21 @@ function App() {
       <Person name={personName} />
       <PersonList names={nameList} /> */}
 
-      <Status status="loading" />
+      {/* <Status status="loading" />
       <Heading>Placeholder text</Heading>
       <Oscar>
         <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
       </Oscar>
-      <Greet name="Vishwas" isLoggedIn={true} />
+      <Greet name="Vishwas" isLoggedIn={true} /> */}
+
+      {/* <Button
+        handleClick={(event, id) => {
+          console.log("Button click", event, id);
+        }}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} /> */}
+
+      {/* <Container styles={{ border: "1px solid black", padding: "1rem" }} /> */}
     </div>
   );
 }
