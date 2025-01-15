42d9aabcc0dd1ce6cfe8015d587093359ceebdfa
Makus14 <mesha.maksim14@gmail.com>
2025-01-15 12:17:09 +0300

    advanced props typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
+// import { Person } from "./components/Person";
+// import { PersonList } from "./components/PersonList";
 import { Greet } from "./components/Greet";
-import { Person } from "./components/Person";
+import { Status } from "./components/Status";
+import { Heading } from "./components/Heading";
+import { Oscar } from "./components/Oscar";
 import "./App.css";
-import { PersonList } from "./components/PersonList";
 
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: "Wayne",
-  };
+  // const personName = {
+  //   first: "Bruce",
+  //   last: "Wayne",
+  // };
 
-  const nameList = [
-    { first: "Bruce", last: "Wayne" },
-    { first: "Clark", last: "Kent" },
-    { first: "Princess", last: "Diana" },
-  ];
+  // const nameList = [
+  //   { first: "Bruce", last: "Wayne" },
+  //   { first: "Clark", last: "Kent" },
+  //   { first: "Princess", last: "Diana" },
+  // ];
 
   return (
     <div className="App">
-      <Greet name="Vishwas" messageCount={20} isLoggedIn={false} />
+      {/* <Greet name="Vishwas" messageCount={20} isLoggedIn={false} />
       <Person name={personName} />
-      <PersonList names={nameList} />
+      <PersonList names={nameList} /> */}
+
+      <Status status="loading" />
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
+      </Oscar>
+      <Greet name="Vishwas" isLoggedIn={true} />
     </div>
   );
 }
