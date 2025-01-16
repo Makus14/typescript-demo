8fa499c4c3387a6478cad58518439c0861c03f75
Makus14 <mesha.maksim14@gmail.com>
2025-01-16 13:43:59 +0300

    contextProvider with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@
 // import { Button } from "./components/Button";
 // import { Input } from "./components/Input";
 // import { Container } from "./components/Container";
+// import { Box } from "./components/context/Box";
+// import { ThemeContextProvider } from "./components/context/ThemeContext";
+import { User } from "./components/context/User";
+import { UserContextProvider } from "./components/context/UserContext";
 import "./App.css";
 
 function App() {
@@ -42,6 +46,14 @@ function App() {
       <Input value="" handleChange={(event) => console.log(event)} /> */}
 
       {/* <Container styles={{ border: "1px solid black", padding: "1rem" }} /> */}
+
+      {/* <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider> */}
+
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
     </div>
   );
 }
