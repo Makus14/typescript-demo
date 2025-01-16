e669ddf083a3fa3a2af3b14f95850f27d201785e
Makus14 <mesha.maksim14@gmail.com>
2025-01-16 15:22:35 +0300

    class, components, generics with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,12 @@
 // import { Container } from "./components/Container";
 // import { Box } from "./components/context/Box";
 // import { ThemeContextProvider } from "./components/context/ThemeContext";
-import { User } from "./components/context/User";
-import { UserContextProvider } from "./components/context/UserContext";
+// import { User } from "./components/context/User";
+// import { UserContextProvider } from "./components/context/UserContext";
+// import { Counter } from "./components/class/Counter";
+// import { Private } from "./components/auth/Private";
+// import { Profile } from "./components/auth/Profile";
+import { List } from "./components/generics/List";
 import "./App.css";
 
 function App() {
@@ -51,9 +55,27 @@ function App() {
         <Box />
       </ThemeContextProvider> */}
 
-      <UserContextProvider>
+      {/* <UserContextProvider>
         <User />
-      </UserContextProvider>
+      </UserContextProvider> */}
+
+      {/* <Counter message="The count value is" /> */}
+
+      {/* <Private isLoggedIn={true} component={Profile} /> */}
+
+      {/* <List
+        items={["Batman", "Superman", "Wonder Woman"]}
+        onClick={(item) => console.log(item)}
+      />
+      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
+      <List
+        items={[
+          { id: 1, first: "Bruce", last: "Wayne" },
+          { id: 2, first: "Clark", last: "Kent" },
+          { id: 3, first: "Princess", last: "Diana" },
+        ]}
+        onClick={(item) => console.log(item)}
+      />
     </div>
   );
 }
